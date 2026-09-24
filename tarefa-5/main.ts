379d94a5ae2785f150d12a68cba98eafb9f713d5
ntorracca <ntorracca@gmail.com>
2026-09-24 15:13:32 -0300

    tarefa-5-concluida

--- tarefa-5/main.ts
+++ tarefa-5/main.ts
@@ -1,7 +1,9 @@
 import ClienteController from "./controller/ClienteController";
 import Cliente from "./entities/Cliente";
+import InMemoryRepository from "./repositories/InMemoryRepository";
+import PostgresRepository from "./repositories/PostgresRepository";
 
-const clienteController = new ClienteController();
+const clienteController = new ClienteController(new PostgresRepository);
 
 const cliente1 = new Cliente(0, "João", "joao@mail.com")
 const cliente2 = new Cliente(0, "Kleber", "kleber@mail.com")
